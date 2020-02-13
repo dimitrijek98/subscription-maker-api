8f6fca5ba3957f2d89fd7cd78de4a7a06a596765
MiticNikola16228 <mitic.nikola@elfak.rs>
2020-02-13 17:43:28 +0100

    login and register

--- index.js
+++ index.js
@@ -8,12 +8,80 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/subscription', {
 	useNewUrlParser: true
-}).then(console.log('bla bla'));
+});
 
 app.use(cors());
 app.listen(port);
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.status(200).send("OK");
+   // let novi = {email: 'novo', password: 'novo'};
+    //User.insert(novi);
+    /*let User = mongoose.connection.db.collection("user");
+    User.find({}).toArray(function(err, data){
+            res.status(200).send(data);
+        }); */  
+        
+});
+
+app.post('/login-admin', (req, res) => {
+    let Adim = mongoose.connection.db.collection("admin");
+    let email = req.body.email;
+    let password = req.body.password;
+    let query = { email: email };
+    Admin.findOne(query, function(err, doc){
+        if(err)
+        {
+            return res.status(500).send();
+        }
+        if(!doc){
+            return res.status(404).send("Admin does not exist!");
+        }
+        if(doc.password != password){
+            return res.status(422).send("Wrong password!");
+        }
+        if(doc.password == password){
+            return res.status(200).send();
+        }
+    });
+});  
+
+app.post('/login-user', (req, res) => {
+    let User = mongoose.connection.db.collection("user");
+    let email = req.body.email;
+    let password = req.body.password;
+    let query = { email: email };
+    User.findOne(query, function(err, doc){
+        if(err)
+        {
+            return res.status(500).send();
+        }
+        if(!doc){
+            return res.status(404).send("User does not exist!");
+        }
+        if(doc.password != password){
+            return res.status(422).send("Wrong password!");
+        }
+        if(doc.password == password){
+            return res.status(200).send();
+        }
+    });
+});
+
+app.post('/signup-user', (req, res) => {
+    let User = mongoose.connection.db.collection("user");
+    let email = req.body.email;
+    let password = req.body.password;
+    let name= req.body.name;
+    let surname= req.body.surname;
+    let adress = req.body.adress;
+    let phone  = req.body.phone;
+    let newUser = { email: email, password: password, name:name, surname:surname, adress:adress, phone:phone};
+    User.insert(newUser, function(err, doc){
+        if(err)
+        {
+            return res.status(500).send();
+        }
+        return res.status(200).send(doc);
+    });
 });
